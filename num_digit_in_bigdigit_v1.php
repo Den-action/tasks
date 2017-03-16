74084b71bfac1f593390312cac42896078f0f485
Den-action <Den-action@yandex.ru>
2017-03-16 09:22:00 +0200

    Update text of task
    Update text of task

--- num_digit_in_bigdigit_v1.php
+++ num_digit_in_bigdigit_v1.php
@@ -1,5 +1,7 @@
 <?php
-//Count num of digit in bigDigit
+//Вам нужно разработать программу, которая считала бы 
+//количество вхождений какой-нибуть выбранной вами 
+//цифры в выбранном вами числе. 
 $digit = $argv[1];
 $bigDigit = $argv[2];
 if (!is_numeric($digit) || !is_numeric($bigDigit))
