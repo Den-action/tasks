a16026edb0455c1cb3112b0cbfe778299c195cda
Den-action <Den-action@yandex.ru>
2017-03-18 19:16:52 +0200

    Refactored file
    Refactored file

--- table.php
+++ table.php
@@ -1,14 +1,21 @@
 <?php
+//$ftable = fopen("table.txt", "w");
+$file = __DIR__ . DIRECTORY_SEPARATOR . 'table.txt';
 for ($i = 1; $i < 10; $i++){
     $str = '';
     for ($j = 1; $j < 10; $j++) {
         echo $str = str_pad($i * $j, 2, ' ', STR-PAD-LEFT) . '|';
+        $res = file_put_contents($file, $str, FILE_APPEND);
+        if (!$res)
+            echo 'Cannot write to ' . $file;
     }
     echo "\n";
+    file_put_contents($file, "\r\n", FILE_APPEND);
     for ($k = 1; $k < 28; $k++){
         echo 0 == $k % 3 ? '+' : '-';
     }
     echo "\n";
 }
+//fclose($ftable);
 
 ?>
