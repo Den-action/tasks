1d9569bbeaaa6dc5d65e351c0d9bffee50f48714
Den-action <Den-action@yandex.ru>
2017-03-20 16:56:12 +0200

    Refactored file
    Refactored file

--- table.php
+++ table.php
@@ -3,19 +3,27 @@ $file = __DIR__ . DIRECTORY_SEPARATOR . 'table.txt';
 for ($i = 1; $i < 10; $i++){
     $str = '';
     for ($j = 1; $j < 10; $j++) {
-        echo $str = str_pad($i * $j, 2, ' ', STR-PAD-LEFT) . '|';
+        echo $str = str_pad($i * $j, 3, ' ', STR-PAD-LEFT);
+        if (1 == $j) {
+            echo '|';
+            $str .= '|';
+        }
         $res = file_put_contents($file, $str, FILE_APPEND);
         if (!$res)
             echo 'Cannot write to ' . $file;
     }
     echo "\n";
     file_put_contents($file, "\r\n", FILE_APPEND);
-    for ($k = 1; $k < 28; $k++){
-        echo $borderStr = (0 == $k % 3) ? '+' : '-';
+    for ($k = 1; $k < 29; $k++) {
+        if (1 == $i) {
+            echo $borderStr = (4 == $k) ? '+' : '-';
+        }
+        else {
+            $borderStr = '';
+        }
         file_put_contents($file, $borderStr, FILE_APPEND);
     }
     echo "\n";
     file_put_contents($file, "\r\n", FILE_APPEND);
 }
-
 ?>
